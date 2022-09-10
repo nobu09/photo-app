17554134a1a954c47bba044a64b297e58d7e1e77
nobu09 <nobu0922@gmail.com>
2022-09-10 17:32:56 +0900

    ログイン機能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,29 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    error_message = []
+    error_message << 'ユーザーIDを入力してください' if session_params[:login].blank?
+    error_message << 'パスワードを入力してください' if session_params[:password].blank?
+    if error_message.present?
+      flash.now[:danger] = error_message
+      render :new and return
+    end
+
+    user = User.find_by(login: session_params[:login])
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to login_path, notice: 'ログインしました'
+    else
+      flash.now[:danger] = ['入力されたユーザーIDとパスワードに一致するユーザーが存在しません']
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:login, :password)
+  end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,26 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  test "ログイン画面" do
+    get login_path
     assert_response :success
+  end
+
+  test "ログインできる" do
+    post login_path, params: { session: { login: "user1", password: "P@ssw0rd" }}
+    assert_redirected_to login_path
+    assert_equal "ログインしました", flash.notice
+  end
+
+  test "ユーザーIDとパスワードが未入力時のエラーメッセージ" do
+    post login_path, params: { session: { login: "", password: "" }}
+    assert_response :success
+    assert_equal %w[ユーザーIDを入力してください パスワードを入力してください], flash.now[:danger]
+  end
+
+  test "ユーザーIDとパスワードが一致するユーザーいない場合のエラーメッセージ" do
+    post login_path, params: { session: { login: "user", password: "P@ssw0rd" }}
+    assert_response :success
+    assert_equal %w[入力されたユーザーIDとパスワードに一致するユーザーが存在しません], flash.now[:danger]
   end
 end
