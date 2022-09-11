20fcf7f25d7c3c1745b83bba009eadabedf7dfbd
nobu09 <nobu0922@gmail.com>
2022-09-11 18:30:53 +0900

    写真投稿機能が動くようにした

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,21 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.all.order(created_at: :desc)
   end
 
   def new
+    @photo = Photo.new
   end
 
   def create
+    photo = Photo.new(photo_params)
+    photo.save!
+    redirect_to photos_path, notice: '写真をアップロードしました'
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :image)
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ApplicationRecord
+  has_one_attached :image
+
   validates :title, presence: true, length: { maximum: 30 }
 end
