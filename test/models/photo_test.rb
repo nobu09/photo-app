222584562c2e3f2f1432f27bcbfad1ad3cf1f0be
nobu09 <nobu0922@gmail.com>
2022-09-11 10:37:23 +0900

    バリデーションのテストを追加

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class PhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "有効な写真" do
+    photo = Photo.new(title: 'あ' * 30)
+    assert photo.valid?
+  end
+
+  test "タイトルなしの場合、無効" do
+    photo = Photo.new(title: '')
+    assert_not photo.valid?
+  end
+
+  test "タイトルが30文字超の場合、無効" do
+    photo = Photo.new(title: 'あ' * 31)
+    assert_not photo.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "有効なユーザー" do
+    user = User.new(login: 'test1', password: 'P@ssw0rd')
+    assert user.valid?
+  end
+
+  test "ユーザーIDなしの場合、無効" do
+    user = User.new(login: '', password: 'P@ssw0rd')
+    assert_not user.valid?
+  end
+
+  test "ユーザーIDが重複している場合、無効" do
+    User.create(login: 'user', password: 'P@ssw0rd')
+    user = User.new(login: 'user', password: 'P@ssw0rd')
+    assert_not user.valid?
+  end
 end
