cd6f7b85337641c5fae4f48311e97967241d3854
nobu09 <nobu0922@gmail.com>
2022-09-14 08:30:30 +0900

    テスト未実装箇所のTODOコメント追加

--- test/controllers/oauth_controller_test.rb
+++ test/controllers/oauth_controller_test.rb
@@ -1,13 +1,5 @@
 require "test_helper"
 
 class OauthControllerTest < ActionDispatch::IntegrationTest
-  test "should get authorize" do
-    get oauth_authorize_url
-    assert_response :success
-  end
-
-  test "should get callback" do
-    get oauth_callback_url
-    assert_response :success
-  end
+  # TODO: テスト実装
 end
