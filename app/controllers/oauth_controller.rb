6c65dea13bcbd2374c6f23d674d16f070c1f9db5
nobu09 <nobu0922@gmail.com>
2022-09-13 22:30:09 +0900

    OAuthアクセストークンの取得

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,13 +1,32 @@
-class OauthController < ApplicationController
-  def authorize
-    redirect_uri = 'http://localhost:3000/oauth/callback'
+require 'net/http'
 
+class OauthController < ApplicationController
+  REDIRECT_URI = 'http://localhost:3000/oauth/callback'
+
+  def authorize
     redirect_to(
-      "#{ENV['ENDPOINT_URL']}/oauth/authorize?client_id=#{ENV["CLIENT_ID"]}&response_type=code&redirect_uri=#{redirect_uri}",
+      "#{ENV['ENDPOINT_URL']}/oauth/authorize?client_id=#{ENV["CLIENT_ID"]}&response_type=code&redirect_uri=#{REDIRECT_URI}",
       allow_other_host: true
     )
   end
 
   def callback
+    uri = URI.parse("#{ENV['ENDPOINT_URL']}/oauth/token")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    req = Net::HTTP::Post.new(uri.path)
+    req.basic_auth(ENV['CLIENT_ID'], ENV['CLIENT_SECRET'])
+    req.set_form_data({
+                        code: params[:code],
+                        client_id: ENV['CLIENT_ID'],
+                        client_secret: ENV['CLIENT_SECRET'],
+                        redirect_uri: REDIRECT_URI,
+                        grant_type: 'authorization_code'
+                      })
+    res = http.request(req)
+    session[:access_token] = JSON.parse(res.body)['access_token']
+
+    redirect_to photos_path
   end
 end
