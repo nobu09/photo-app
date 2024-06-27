ec3d89b0838a1dcf1214bfec0b1dde93f8d73721
nobu09 <nobu09@users.noreply.github.com>
2024-06-27 02:33:15 +0000

    Autocorrect `Lint/UnusedBlockArgument`
    ## Summary
    
    Autocorrected [Lint/UnusedBlockArgument](https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Lint/UnusedBlockArgument).
    
    ## Details
    
    ### Lint/UnusedBlockArgument
    
    > <div class="paragraph">
    > <p>Checks for unused block arguments.</p>
    > </div>
    >
    > #### Examples
    >
    > ```ruby
    > # bad
    > do_something do |used, unused|
    >   puts used
    > end
    >
    > do_something do |bar|
    >   puts :foo
    > end
    >
    > define_method(:foo) do |bar|
    >   puts :baz
    > end
    >
    > # good
    > do_something do |used, _unused|
    >   puts used
    > end
    >
    > do_something do
    >   puts :foo
    > end
    >
    > define_method(:foo) do |_bar|
    >   puts :baz
    > end
    > ```
    >
    > IgnoreEmptyBlocks: true (default)
    >
    > ```ruby
    > # good
    > do_something { |unused| }
    > ```
    >
    > IgnoreEmptyBlocks: false
    >
    > ```ruby
    > # bad
    > do_something { |unused| }
    > ```
    >
    > AllowUnusedKeywordArguments: false (default)
    >
    > ```ruby
    > # bad
    > do_something do |unused: 42|
    >   foo
    > end
    > ```
    >
    > AllowUnusedKeywordArguments: true
    >
    > ```ruby
    > # good
    > do_something do |unused: 42|
    >   foo
    > end
    > ```
    
    ### Note
    
    This pull request was generated by [rubocop_todo_corrector](https://github.com/r7kamura/rubocop_todo_corrector).

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module NewPhotoApp
     config.active_storage.variant_processor = :mini_magick
 
     # field_with_errorsによるレイアウト崩れを避けるために設定
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    config.action_view.field_error_proc = Proc.new { |html_tag, _instance| html_tag }
   end
 end
